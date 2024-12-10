a18ac8448c8ac1d05e272de6576ecb396cb4523a
zephane123 <igopbraga@gmail.com>
2024-12-10 19:31:29 +0800

    Add files via upload

---
+++ taxi-api/modules/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once "Common.php";
+
+class Post extends Common{
+
+    protected $pdo;
+
+    public function __construct(\PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    public function postStudents(){
+        //code for retrieving data on DB
+        return "This is some student records added.";
+    }
+
+    public function postClasses(){
+        //code for retrieving data on DB
+        return "This is some classes records added.";
+    }
+
+    public function postFaculty(){
+        //code for retrieving data on DB
+        return "This is some faculty records added.";
+    }
+
+
+    public function postCar($body){
+      if(is_array($body))
+      {
+        $body = (object) $body;
+      }
+      $result = $this->postData("car_tbl", ["fname" => $body->fname, "lname" => $body->lname], $this->pdo);
+      if ($result ['code']==200) {
+            $this->logger("Kaliwangbetlognizeph", "POST", "Created a new car record.");
+            return $this->generateResponse($result['data'], "success", "New car record has been successfully created.", 201);
+        }
+        $this->logger("Kaliwangbetlognizeph", "POST", $result['errmsg']);
+        return $this->generateResponse(null, "failed", $result['errmsg'], $result['code']);
+    }
+}
+
+?>
